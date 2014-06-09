e72358ccc965843ea0a6471f2a6305b424423687
lal <narayanp@360logica.com>
2014-06-09 19:47:02 +0530

    Testd

--- SingleAPdkkd/Main.cs
+++ SingleAPdkkd/Main.cs
@@ -15,6 +15,7 @@ namespace SingleAPdkkd
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
-		}
+			//Test
+		}aa
 	}
 }
